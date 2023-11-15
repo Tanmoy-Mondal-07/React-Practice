d90882ee4abcf58fe7828d5e9dbb33fc34e44037
Tanmoy <tanmoy10122000@gmail.com>
2023-11-15 08:38:12 +0530

    pd

--- Notebook/backend/routes/auth.js
+++ Notebook/backend/routes/auth.js
@@ -1,12 +1,15 @@
 const express = require('express');
-const router = express.Router()
+const User = require('../models/User');
+const router = express.Router();
 
 router.get('/', (req, res) => {
-obj = {
-    a: 'thanos',
-    number: 34
-}
-    res.json(obj)
+
+console.log(req.body);
+const user = User(req.body);
+user.save();
+res.send(req.body);
+
+
 })
 
 module.exports = router
