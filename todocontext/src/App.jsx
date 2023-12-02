fc7d740ca2b796d590ca9404e2f0e8eb4b807f68
Tanmoy <tanmoy10122000@gmail.com>
2023-12-02 19:50:01 +0530

    todocontext

--- todocontext/src/App.jsx
+++ todocontext/src/App.jsx
@@ -5,13 +5,37 @@ import './App.css'
 import { Todoprovider } from './context/TodoContext'
 
 function App() {
-  const [ak, setak] = useState("jdjtdj")
+  const [todos, settodos] = useState([])
+
+  const addtodo =(todo)=>{
+    settodos((pr)=>[...pr, {id:Date.now(), ...todo}])
+  }
+
+  const updatedtodo=(id, todo)=>{
+settodos((pr)=> pr.map((pt)=>(pt.id===id ? todo : pt)))
+  }
+
+  const deletetodo=(id)=>{
+settodos((pr)=> pr.filter((todo)=>todo.id !== id))
+  }
+
+const togglecomplete=(id)=>{
+  
+}
 
   return (
-    <Todoprovider>
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <Todoprovider value={{todos, addtodo, updatedtodo, deletetodo,togglecomplete}}>
+    <div className="bg-[#172842] min-h-screen py-8">
+                <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
+                    <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
+                    <div className="mb-4">
+                        {/* Todo form goes here */} 
+                    </div>
+                    <div className="flex flex-wrap gap-y-3">
+                        {/*Loop and Add TodoItem here */}
+                    </div>
+                </div>
+            </div>
     </Todoprovider>
   )
 }
