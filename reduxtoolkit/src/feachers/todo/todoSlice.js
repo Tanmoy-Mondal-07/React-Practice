8d1655ba82e058899a5caaf3bc122af502103e3b
Tanmoy <tanmoy10122000@gmail.com>
2023-12-10 19:09:54 +0530

    redux

---
+++ reduxtoolkit/src/feachers/todo/todoSlice.js
@@ -0,0 +1,24 @@
+import { createSlice,nanoid } from "@reduxjs/toolkit";
+
+const initialState={
+    todos:[{id: 1, text:"hello world"}]
+}
+
+export const todoSlice = createSlice({
+    name: 'todo',
+    initialState,
+    reducers:{
+        addtodo:(state,action)=>{
+            const todo = {
+                id: nanoid(),
+                text:action.payload,
+            }
+            state.todos.push(todo)
+        },
+        removetodo:()=>{
+            state.todos=state.todos.filter((todo)=>todo.id !== action.payload)
+        },
+    }
+})
+
+export const {addtodo,removetodo}=todoSlice.actions
