e1315ec7aaf86baaa698311bd793174844833731
Tanmay Mondal <tanmoy10122000@gmail.com>
2025-06-30 21:01:29 +0530

    ..

--- ReactVideoPlayer/vite-project/src/App.jsx
+++ ReactVideoPlayer/vite-project/src/App.jsx
@@ -8,7 +8,17 @@ function App() {
 
   return (
     <>
-      <video style={{height:"250px"}} src='https://ik.imagekit.io/kduhx1jnl/yt/278750_medium.mp4?updatedAt=1751291850371' />
+      <video src='https://ik.imagekit.io/kduhx1jnl/yt/278750_medium.mp4?updatedAt=1751291850371?tr=h-250'
+        height="250px"
+        controls
+        // autoPlay
+        // muted
+        // loop
+        poster='https://ik.imagekit.io/kduhx1jnl/yt/278750_medium.mp4/ik-thumbnail.jpg?tr=so-5'
+      >
+        <track kind='subtitles' src='/english.vtt' srcLang='en' label='English' default />
+        <track kind='subtitles' src='/hindi.vtt' srcLang='hi' label='Hindi' />
+      </video>
     </>
   )
 }
