ebbc078e29384f40121f3a9f54f726bff50a8eaf
Tanmay Mondal <tanmoy10122000@gmail.com>
2025-07-01 19:27:14 +0530

    ..

--- ReactVideoPlayer/vite-project/src/App.jsx
+++ ReactVideoPlayer/vite-project/src/App.jsx
@@ -1,11 +1,7 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  
   return (
     <>
       <video src='https://ik.imagekit.io/kduhx1jnl/yt/278750_medium.mp4?updatedAt=1751291850371?tr=h-250'
