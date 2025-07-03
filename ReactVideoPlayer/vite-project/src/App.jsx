f1f02760d0508fc1e8c97641caf30bc7f27c6626
Tanmay Mondal <tanmoy10122000@gmail.com>
2025-07-03 20:46:37 +0530

    ..

--- ReactVideoPlayer/vite-project/src/App.jsx
+++ ReactVideoPlayer/vite-project/src/App.jsx
@@ -1,11 +1,14 @@
 import './App.css'
 import Videojs from './components/videojs'
+import VideoJSPlayer from "./components/videojs-player";
+import ImageKitBasicPlayer from "./components/imagekit-player";
+import BasicVideoPlayer from "./components/video-player";
 
 function App() {
 
   return (
-    <>
-      <Videojs />
+    <div className="App">
+      <VideoJSPlayer />
 
       {/* <video src='https://ik.imagekit.io/kduhx1jnl/yt/278750_medium.mp4?updatedAt=1751291850371?tr=h-250'
         height="250px"
@@ -18,7 +21,7 @@ function App() {
         <track kind='subtitles' src='/english.vtt' srcLang='en' label='English' default />
         <track kind='subtitles' src='/hindi.vtt' srcLang='hi' label='Hindi' />
       </video> */}
-    </>
+    </div>
   )
 }
 
