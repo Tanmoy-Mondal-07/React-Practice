17325d193b141922e8b75679245822ca66929495
Tanmay Mondal <tanmoy10122000@gmail.com>
2025-07-06 20:04:45 +0530

    ..

--- stickyNote/src/App.jsx
+++ stickyNote/src/App.jsx
@@ -2,19 +2,19 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-const notes = [
-  {
-    id: 1,
-    text: "Check the description for my Frontent Prep"
-  },
-  {
-    id:2,
-    text: "Star the repo"
-  }
-]
+  const [notes, setnotes] = useState([
+    {
+      id: 1,
+      text: "Check the description for my Frontent Prep"
+    },
+    {
+      id: 2,
+      text: "Star the repo"
+    }
+  ])
   return (
     <div>
-      
+      <Notes notes={notes} setnotes={setnotes}/>
     </div>
   )
 }
