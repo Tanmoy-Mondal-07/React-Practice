414e5a89d9ab7171158a0309af1f2991fb8836e2
Tanmay Mondal <tanmoy10122000@gmail.com>
2025-07-06 19:13:39 +0530

    ..

--- stickyNote/src/App.jsx
+++ stickyNote/src/App.jsx
@@ -2,12 +2,20 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+const notes = [
+  {
+    id: 1,
+    text: "Check the description for my Frontent Prep"
+  },
+  {
+    id:2,
+    text: "Star the repo"
+  }
+]
   return (
-    <>
+    <div>
       
-    </>
+    </div>
   )
 }
 
