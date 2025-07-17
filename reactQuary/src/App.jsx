3a23e52a16e15a7e90bf9ee1d43d97a625faddf2
Tanmay Mondal <tanmoy10122000@gmail.com>
2025-07-17 20:29:46 +0530

    ..

--- reactQuary/src/App.jsx
+++ reactQuary/src/App.jsx
@@ -1,8 +1,14 @@
 import { useState } from 'react'
 import './App.css'
+import { useQuery } from '@tanstack/react-query'
 
 function App() {
   const [count, setCount] = useState(0)
+
+  useQuery({
+    queryKey:["posts"],
+    queryFn
+  })
 
   return (
     <>
--- reactQuary/src/main.jsx
+++ reactQuary/src/main.jsx
@@ -2,12 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { QueryClient } from '@tanstack/react-query'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 const queryClient = QueryClient()
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </StrictMode>,
 )
