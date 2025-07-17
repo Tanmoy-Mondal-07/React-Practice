a54a9c8ec3926885fdaa5d1cf079bf97fe9f17f8
Tanmay Mondal <tanmoy10122000@gmail.com>
2025-07-17 19:54:35 +0530

    ..

--- reactQuary/src/main.jsx
+++ reactQuary/src/main.jsx
@@ -2,6 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { QueryClient } from '@tanstack/react-query'
+
+const queryClient = QueryClient()
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
