d70fbd81b2aaa8dc79b5e62538638ac2a75621b8
Tanmay Mondal <tanmoy10122000@gmail.com>
2025-07-21 20:32:35 +0530

    ..

--- reactQuary/src/components/post.list.jsx
+++ reactQuary/src/components/post.list.jsx
@@ -1,15 +1,43 @@
-import { useQuery } from '@tanstack/react-query'
+import { useMutation, useQuery } from '@tanstack/react-query'
 import React from 'react'
-import { fetchPosts } from '../api/api'
+import { addPost, fetchPosts } from '../api/api'
 
 const PostList = () => {
     const { data: postData, isError, isLoading, error } = useQuery({
         queryKey: ["posts"],
         queryFn: fetchPosts,
-
     })
+
+    const { mutate, isError: isPostError, isPending, error: postError, reset } = useMutation({
+        mutationFn: addPost,
+    })
+
     return (
         <div className='container'>
+            
+            <form onSubmit={handleSubmit}>
+                {isPostError && <h5 onClick={() => reset()}>Unable to Post</h5>}
+                <input
+                    type="text"
+                    placeholder="Enter your post.."
+                    className="postbox"
+                    name="title"
+                />
+                <div className="tags">
+                    {tagsData?.map((tag) => {
+                        return (
+                            <div key={tag}>
+                                <input name={tag} id={tag} type="checkbox" />
+                                <label htmlFor={tag}>{tag}</label>
+                            </div>
+                        );
+                    })}
+                </div>
+                <button disabled={isPending}>
+                    {isPending ? "Posting..." : "Post"}
+                </button>
+            </form>
+
             {isLoading && <p>loading...</p>}
             {isError && <p>{error?.message}</p>}
 
@@ -17,7 +45,6 @@ const PostList = () => {
                 return (<div key={post.id} className='post'>
                     <div>{post.title}</div>
                     {post.tags.map((tag) => {
-                        
                         return <span key={tag}>{tag}</span>;
                     })}
                 </div>)
